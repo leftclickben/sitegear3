de2c8b167061b4d9df17fe9a9cb786e087c2d377
Ben New <ben@leftclick.com.au>
2014-01-28 00:56:21 +0800

    Gruntfile: add jslint of all source, and complete jasmine tests configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,17 +8,47 @@
 (function () {
 	"use strict";
 	module.exports = function (grunt) {
-
+		grunt.loadNpmTasks('grunt-jasmine-node');
+		grunt.loadNpmTasks('grunt-jslint');
 		grunt.initConfig({
 			pkg: grunt.file.readJSON('package.json'),
+			jslint: {
+				root: {
+					src: [
+						'index.js',
+						'Gruntfile.js'
+					],
+					options: {
+						log: 'build/log/jslint/jslint.root.log'
+					}
+				},
+				lib: {
+					src: [
+						'lib/**/*.js'
+					],
+					options: {
+						log: 'build/log/jslint/jslint.src.log'
+					}
+				},
+				tests: {
+					src: [
+						'tests/**/*.js'
+					],
+					options: {
+						log: 'build/log/jslint/jslint.tests.log'
+					}
+				}
+			},
 			jasmine_node: {
-				projectRoot: '.'
+				projectRoot: '.',
+				jUnit: {
+					report: true,
+					savePath: 'build/log/jasmine/'
+				},
+				forceExit: false
 			}
 		});
-
-		grunt.loadNpmTasks('grunt-jasmine-node');
-
-		grunt.registerTask('default', ['jasmine_node'])
-
+		grunt.registerTask('qa', ['jasmine_node', 'jslint']);
+		grunt.registerTask('default', ['qa']);
 	};
 }());
