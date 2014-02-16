5f3effeb4f9472e77f5915ce7f6c42d140acca39
Ben New <ben@leftclick.com.au>
2014-02-16 13:54:23 +0800

    Improve async usage in tests.

--- tests/unit/utils/globGetters.spec.js
+++ tests/unit/utils/globGetters.spec.js
@@ -13,103 +13,95 @@
 	describe('Utility Function: globGetters()', function () {
 		describe('Uses __defineGetter__()', function () {
 			var object;
-			beforeEach(function () {
+			beforeEach(function (done) {
 				object = {};
 				spyOn(object, '__defineGetter__').andCallThrough();
-			});
-			it('Uses __defineGetter__() to define getters', function (done) {
 				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.__defineGetter__).toHaveBeenCalled();
-					expect(object.__defineGetter__.callCount).toBe(2);
 					done();
 				});
+			});
+			it('Uses __defineGetter__() to define getters', function () {
+				expect(object.__defineGetter__).toHaveBeenCalled();
+				expect(object.__defineGetter__.callCount).toBe(2);
 			});
 		});
 		describe('Creates an empty object by default', function () {
-			it('Loads all .js files in specified directory and unpacks them as getters', function (done) {
-				utils.globGetters(path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject.a).toBeTruthy();
-					expect(_.isFunction(returnedObject.a)).toBeTruthy();
-					expect(returnedObject.b).toBeTruthy();
-					expect(_.isFunction(returnedObject.b)).toBeTruthy();
+			var error, returnedObject;
+			beforeEach(function (done) {
+				utils.globGetters(path.join(__dirname, '_getters', '*.js'), {}, function (e, r) {
+					error = e;
+					returnedObject = r;
 					done();
 				});
 			});
-			it('Doesn\'t define properties based on non-js files', function (done) {
-				utils.globGetters(path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject.x).toBeUndefined();
-					done();
-				});
+			it('Does not return an error', function () {
+				expect(error).toBeFalsy();
 			});
-			it('Doesn\'t define any additional properties', function (done) {
-				utils.globGetters(path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					expect(_.size(returnedObject)).toBe(2);
-					done();
-				});
+			it('Loads all .js files in specified directory and unpacks them as getters', function () {
+				expect(returnedObject.a).toBeTruthy();
+				expect(_.isFunction(returnedObject.a)).toBeTruthy();
+				expect(returnedObject.b).toBeTruthy();
+				expect(_.isFunction(returnedObject.b)).toBeTruthy();
 			});
-			it('Defines getters that work as expected', function (done) {
-				utils.globGetters(path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					var a = returnedObject.a(),
-						b = returnedObject.b();
-					expect(_.isFunction(a)).toBeTruthy();
-					expect(a()).toBe('This is "a"');
-					expect(_.isFunction(b)).toBeTruthy();
-					expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
-					expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
-					done();
-				});
+			it('Doesn\'t define properties based on non-js files', function () {
+				expect(returnedObject.x).toBeUndefined();
+			});
+			it('Doesn\'t define any additional properties', function () {
+				expect(_.size(returnedObject)).toBe(2);
+			});
+			it('Defines getters that work as expected', function () {
+				var a = returnedObject.a(),
+					b = returnedObject.b();
+				expect(_.isFunction(a)).toBeTruthy();
+				expect(a()).toBe('This is "a"');
+				expect(_.isFunction(b)).toBeTruthy();
+				expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
+				expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
 			});
 		});
 		describe('Extends an empty object and returns it', function () {
-			var object;
-			beforeEach(function () {
+			var object, error, returnedObject;
+			beforeEach(function (done) {
 				object = {};
-			});
-			it('Returns the passed-in object', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject).toBe(object);
+				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function (e, r) {
+					error = e;
+					returnedObject = r;
 					done();
 				});
 			});
-			it('Loads all .js files in specified directory and unpacks them as getters', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.a).toBeTruthy();
-					expect(_.isFunction(object.a)).toBeTruthy();
-					expect(object.b).toBeTruthy();
-					expect(_.isFunction(object.b)).toBeTruthy();
-					done();
-				});
+			it('Does not return an error', function () {
+				expect(error).toBeFalsy();
 			});
-			it('Doesn\'t define properties based on non-js files', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.x).toBeUndefined();
-					done();
-				});
+			it('Returns the passed-in object', function () {
+				expect(returnedObject).toBe(object);
 			});
-			it('Doesn\'t define any additional properties', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(_.size(object)).toBe(2);
-					done();
-				});
+			it('Loads all .js files in specified directory and unpacks them as getters', function () {
+				expect(object.a).toBeTruthy();
+				expect(_.isFunction(object.a)).toBeTruthy();
+				expect(object.b).toBeTruthy();
+				expect(_.isFunction(object.b)).toBeTruthy();
 			});
-			it('Defines getters that work as expected', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					var a = object.a(),
-						a2 = object.a(),
-						b = object.b();
-					expect(_.isFunction(a)).toBeTruthy();
-					expect(a2).not.toBe(a);
-					expect(a()).toBe('This is "a"');
-					expect(_.isFunction(b)).toBeTruthy();
-					expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
-					expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
-					done();
-				});
+			it('Doesn\'t define properties based on non-js files', function () {
+				expect(object.x).toBeUndefined();
+			});
+			it('Doesn\'t define any additional properties', function () {
+				expect(_.size(object)).toBe(2);
+			});
+			it('Defines getters that work as expected', function () {
+				var a = object.a(),
+					a2 = object.a(),
+					b = object.b();
+				expect(_.isFunction(a)).toBeTruthy();
+				expect(a2).not.toBe(a);
+				expect(a()).toBe('This is "a"');
+				expect(_.isFunction(b)).toBeTruthy();
+				expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
+				expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
 			});
 		});
 		describe('Extends an existing object and returns it', function () {
-			var object;
-			beforeEach(function () {
+			var object, error, returnedObject;
+			beforeEach(function (done) {
 				object = {
 					a: function () {
 						return 'This is pre-existing "a"';
@@ -119,87 +111,76 @@
 					},
 					z: 'A string'
 				};
-			});
-			it('Returns the passed-in object', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject).toBe(object);
+				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function (e, r) {
+					error = e;
+					returnedObject = r;
 					done();
 				});
 			});
-			it('Overrides existing properties with a new getter', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.a).toBeTruthy();
-					expect(_.isFunction(object.a)).toBeTruthy();
-					var a = object.a();
-					expect(_.isFunction(a)).toBeTruthy();
-					expect(a()).toBe('This is "a"');
-					done();
-				});
+			it('Does not return an error', function () {
+				expect(error).toBeFalsy();
 			});
-			it('Extends the object with new getters', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.b).toBeTruthy();
-					expect(_.isFunction(object.b)).toBeTruthy();
-					var b = object.b();
-					expect(_.isFunction(b)).toBeTruthy();
-					expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
-					expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
-					done();
-				});
+			it('Returns the passed-in object', function () {
+				expect(returnedObject).toBe(object);
 			});
-			it('Doesn\'t define properties based on non-js files', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.x).toBeUndefined();
-					done();
-				});
+			it('Overrides existing properties with a new getter', function () {
+				expect(object.a).toBeTruthy();
+				expect(_.isFunction(object.a)).toBeTruthy();
+				var a = object.a();
+				expect(_.isFunction(a)).toBeTruthy();
+				expect(a()).toBe('This is "a"');
 			});
-			it('Doesn\'t define any additional properties', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(_.size(object)).toBe(4); // 2 existing not overridden, 1 existing overridden, 1 new
-					done();
-				});
+			it('Extends the object with new getters', function () {
+				expect(object.b).toBeTruthy();
+				expect(_.isFunction(object.b)).toBeTruthy();
+				var b = object.b();
+				expect(_.isFunction(b)).toBeTruthy();
+				expect(b('a parameter')).toBe('This is "b", parameter is "a parameter"');
+				expect(b('another parameter')).toBe('This is "b", parameter is "another parameter"');
 			});
-			it('Doesn\'t affect other pre-existing properties', function (done) {
-				utils.globGetters(object, path.join(__dirname, '_getters', '*.js'), {}, function () {
-					expect(object.y()).toBe('This is pre-existing "y"');
-					expect(object.z).toBe('A string');
-					done();
-				});
+			it('Doesn\'t define properties based on non-js files', function () {
+				expect(object.x).toBeUndefined();
+			});
+			it('Doesn\'t define any additional properties', function () {
+				expect(_.size(object)).toBe(4); // 2 existing not overridden, 1 existing overridden, 1 new
+			});
+			it('Doesn\'t affect other pre-existing properties', function () {
+				expect(object.y()).toBe('This is pre-existing "y"');
+				expect(object.z).toBe('A string');
 			});
 		});
 		describe('Matches deep glob patterns', function () {
-			it('Loads all .js files in specified directory and unpacks them as getters', function (done) {
-				utils.globGetters(/\/([a-zA-Z0-9_\-\.]+)\/getter\.js/, path.join(__dirname, '_gettersNested', '**', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject.m).toBeTruthy();
-					expect(_.isFunction(returnedObject.m)).toBeTruthy();
-					expect(returnedObject.n).toBeTruthy();
-					expect(_.isFunction(returnedObject.n)).toBeTruthy();
+			var error, returnedObject;
+			beforeEach(function (done) {
+				utils.globGetters(/\/([a-zA-Z0-9_\-\.]+)\/getter\.js/, path.join(__dirname, '_gettersNested', '**', '*.js'), {}, function (e, r) {
+					error = e;
+					returnedObject = r;
 					done();
 				});
 			});
-			it('Doesn\'t define properties based on non-js files', function (done) {
-				utils.globGetters(/\/([a-zA-Z0-9_\-\.]+)\/getter\.js/, path.join(__dirname, '_gettersNested', '**', '*.js'), {}, function (error, returnedObject) {
-					expect(returnedObject.x).toBeUndefined();
-					done();
-				});
+			it('Does not return an error', function () {
+				expect(error).toBeFalsy();
 			});
-			it('Doesn\'t define any additional properties', function (done) {
-				utils.globGetters(/\/([a-zA-Z0-9_\-\.]+)\/getter\.js/, path.join(__dirname, '_gettersNested', '**', '*.js'), {}, function (error, returnedObject) {
-					expect(_.size(returnedObject)).toBe(2);
-					done();
-				});
+			it('Loads all .js files in specified directory and unpacks them as getters', function () {
+				expect(returnedObject.m).toBeTruthy();
+				expect(_.isFunction(returnedObject.m)).toBeTruthy();
+				expect(returnedObject.n).toBeTruthy();
+				expect(_.isFunction(returnedObject.n)).toBeTruthy();
 			});
-			it('Defines getters that work as expected', function (done) {
-				utils.globGetters(/\/([a-zA-Z0-9_\-\.]+)\/getter\.js/, path.join(__dirname, '_gettersNested', '**', '*.js'), {}, function (error, returnedObject) {
-					var m = returnedObject.m(),
-						n = returnedObject.n();
-					expect(_.isFunction(m)).toBeTruthy();
-					expect(m()).toBe('This is "m"');
-					expect(_.isFunction(n)).toBeTruthy();
-					expect(n('a parameter')).toBe('This is "n", parameter is "a parameter"');
-					expect(n('another parameter')).toBe('This is "n", parameter is "another parameter"');
-					done();
-				});
+			it('Doesn\'t define properties based on non-js files', function () {
+				expect(returnedObject.x).toBeUndefined();
+			});
+			it('Doesn\'t define any additional properties', function () {
+				expect(_.size(returnedObject)).toBe(2);
+			});
+			it('Defines getters that work as expected', function () {
+				var m = returnedObject.m(),
+					n = returnedObject.n();
+				expect(_.isFunction(m)).toBeTruthy();
+				expect(m()).toBe('This is "m"');
+				expect(_.isFunction(n)).toBeTruthy();
+				expect(n('a parameter')).toBe('This is "n", parameter is "a parameter"');
+				expect(n('another parameter')).toBe('This is "n", parameter is "another parameter"');
 			});
 		});
 	});
